58bd40454366d742e539f968601fcc11936dba63
mark <markpackham1@gmail.com>
2021-06-29 16:09:16 +0100

    toggle add/close for form, https://youtu.be/w7ejDZ8SWv8?t=4788

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -46,10 +47,15 @@ function App() {
     );
   };
 
+  // && is a shorthand way of doing a terniary so we hide AddTask form till it is needed
+
   return (
     <div className="container">
-      <Header />
-      <AddTask onAdd={addTask} />
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
