55ec7e2bfa73829e8513a0199e930c44bd573670
mark <markpackham1@gmail.com>
2021-06-29 09:52:30 +0100

    add button, https://youtu.be/w7ejDZ8SWv8?t=2277

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,21 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+
 const Header = ({title}) => {
     return (
-        <header>
+        <header className="header">
             <h1>{title}</h1>
+            <Button color='green' text='Add' />
         </header>
     )
 }
 
 Header.defaultProps = {
-    title: 'Task Tracker'
+    title: 'Task Tracker',
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
 }
 
 export default Header
